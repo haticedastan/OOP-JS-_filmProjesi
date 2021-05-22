288dc03af8b9eccfff6d33b5571cbe3155a1b201
Hatice <40773971+haticedastan@users.noreply.github.com>
2021-05-22 15:17:00 +0300

    All film load

--- project.js
+++ project.js
@@ -16,6 +16,11 @@ eventListeners();
 
 function eventListeners(){
     form.addEventListener("submit",addFilm);
+
+    document.addEventListener("DOMContentLoaded",function(){
+        let films=storage.getFilmsFromStorage();
+        ui.loadAllFilms(films);
+    });
 }
 
 function addFilm(e){
--- ui.js
+++ ui.js
@@ -39,5 +39,21 @@ UI.prototype.addFilmToUI=function(newFilm){
         setTimeout(() => {
             div.remove();
         }, 1000);
-        
+
+    }
+
+    UI.prototype.loadAllFilms=function(films){
+        const filmList=document.getElementById("films");
+        films.forEach(function(film){
+            filmList.innerHTML+=
+         `   
+         <tr>
+         <td><img src="${film.url}" class="img-fluid img-thumbnail"></td>
+         <td>${film.title}</td>
+         <td>${film.director}</td>
+         <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+      </tr>
+         `
+
+        }) 
     }
