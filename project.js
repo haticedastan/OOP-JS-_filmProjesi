40f48dcf661fb03fcff4770f469a003140aceda0
Hatice <40773971+haticedastan@users.noreply.github.com>
2021-05-22 14:58:45 +0300

    localStorage

---
+++ project.js
@@ -0,0 +1,43 @@
+const form= document.getElementById("film-form");
+const titleElement = document.querySelector("#title");
+const directorElement=document.querySelector("#director");
+const urlElement=document.querySelector("#url");
+
+
+//UI objesini başlatma
+
+const ui=new UI();
+//sTORAGE
+const storage =new Storage();
+
+//tüm eventleri yükleme
+
+eventListeners();
+
+function eventListeners(){
+    form.addEventListener("submit",addFilm);
+}
+
+function addFilm(e){
+    const title=titleElement.value;
+    const director=directorElement.value;
+    const url=urlElement.value;
+
+    if(title==="" || director==="" || url===""){
+        ui.displayMessages("tüm alanları doldurun...","danger");
+        
+    }
+
+    else{
+        const newFilm=new Film(title,director,url);
+        ui.addFilmToUI(newFilm);
+
+        storage.addFilmToStorage(newFilm);
+        ui.displayMessages("film başarıyla eklendi...","success");
+        
+    }
+
+
+    ui.clearInputs(titleElement,urlElement,directorElement);
+    e.preventDefault();
+}
